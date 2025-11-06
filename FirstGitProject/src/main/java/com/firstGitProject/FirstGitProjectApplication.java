3d33a940b0693008072f3132352b220da16efa71
SiddeshUB <ubsiddesh@gmail.com>
2025-11-06 12:27:34 +0530

    My git first project

---
+++ FirstGitProject/src/main/java/com/firstGitProject/FirstGitProjectApplication.java
@@ -0,0 +1,11 @@
+package com.firstGitProject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class FirstGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitProjectApplication.class, args);
+		System.out.println("My project");
+	}
+}
